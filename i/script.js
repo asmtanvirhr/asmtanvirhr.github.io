caa630969637db0a0bacece229eb1c08b905d0c4
A S M Tanvir Hasan <75176412+asmtanvirhr@users.noreply.github.com>
2021-03-15 13:57:04 +0600

    b

---
+++ i/script.js
@@ -0,0 +1,20 @@
+(function ($) {
+	$(window).on("load", function () {
+		$.instagramFeed({
+			username: "asmtanvirhr",
+			container: "#instagram-feed",
+			display_profile: false,
+			display_biography: false,
+			display_gallery: true,
+			display_captions: false,
+			max_tries: 8,
+			callback: null,
+			styling: false,
+			items: 12,
+			items_per_row: 4,
+			margin: 1,
+			lazy_load: true,
+			on_error: console.error
+		});
+	});
+})(jQuery);
